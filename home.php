7c96bc3873af0bdec77c443eacd65ceb11a1f1da
JubiraTamu <jubirakhanam2000@gmail.com>
2024-09-24 12:03:33 +0600

    Add files via upload
    This project i have used php

---
+++ home.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant page</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="short cut icon" href="https://cdn-icons-png.flaticon.com/512/5787/5787016.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+</head>
+
+<body>
+    <div class="header-section">
+        <div class="foodie">
+            <h1> <a href="#">Foodie<span>.</span></a></h1>
+        </div>
+        <div class="nav-bar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        <div class="search-button-section">
+            <a href="#"><i class="fa fa-search"></i></a>
+            <a href="#"><button class="btn">Reservation</button></a>
+        </div>
+    </div>
+    <hr>
+
+    <main style="background-image: url(https://codewithsadee.github.io/foodie/assets/images/hero-bg.jpg)">
+        <div class="col-s-6 heading-section">
+            <p class="hero-suiper">Eat Sleep And</p>
+            <h1 class="subpper-section">Supper delicious <br> Burger in town!</h1>
+            <p class="hero-suipers">
+                Food is any substance consumed to provide nutritional <br> support for an orgaism.
+            </p>
+            <button class="bt-btn">Book A Table</button>
+        </div>
+        <div class="col-s-4 heading-img-section">
+            <img src="https://codewithsadee.github.io/foodie/assets/images/hero-banner.png" alt="Burger">
+        </div>
+    </main>
+
+    <div class="card-section">
+        <footer>
+            <div class="card-section">
+                <div class="card">
+                    <div class="icon">
+                        <h1 class="fa  fa-pizza-slice "></h1>
+                    </div>
+                    <h3>Maxican Pizza</h3>
+                    <p>Food is any substance consumed to provide nutritional support for an organism.</p>
+                    <img src="https://codewithsadee.github.io/foodie/assets/images/promo-1.png" alt="">
+                </div>
+            </div>
+            <div class="card-section">
+                <div class="card">
+                    <div class="icon">
+                        <h1 class="fa fa-champagne-glasses"></h1>
+                    </div>
+                    <h3>Soft Drinks</h3>
+                    <p>Food is any substance consumed to provide nutritional support for an organism.</p>
+                    <img src="https://codewithsadee.github.io/foodie/assets/images/promo-2.png" alt="">
+                </div>
+            </div>
+            <div class="card-section">
+                <div class="card">
+                    <div class="icon">
+                        <h1 class="fa-solid fa-drumstick-bite"></h1>
+                    </div>
+                    <h3>Chicken Fry</h3>
+                    <p>Food is any substance consumed to provide nutritional support for an organism.</p>
+                    <img src="https://codewithsadee.github.io/foodie/assets/images/promo-3.png" alt="">
+                </div>
+            </div>
+            <div class="card-section">
+                <div class="card">
+                    <div class="icon">
+                        <h1 class="fa fa-burger"></h1>
+                    </div>
+                    <h3>Burger Kingo</h3>
+                    <p>Food is any substance consumed to provide nutritional support for an organism.</p>
+                    <img src="https://codewithsadee.github.io/foodie/assets/images/promo-1.png" alt="">
+                </div>
+            </div>
+        </footer>
+    </div>
+    <div class="caferio-section">
+        <div class="col-s-5 caferio-image-section">
+            <img src="https://codewithsadee.github.io/foodie/assets/images/about-banner.png" alt="barger-image">
+        </div>
+        <div class="col-s-5 caferio-suggest-section">
+            <h1>
+                Caferio, Burgers, and <br> Best Pizzas <span> in Town!</span>
+            </h1>
+            <p>The restaurants in Hangzhou also catered to many northern Chinese who had fled south from Kaifeng during the Jurchen invasion of the 1120s, while it is also known that many restaurants were run by families.</p>
+            <ul>
+                <li>Delicious & Healthy Foods</li>
+                <li>Spacific Family And Kids Zone</li>
+                <li>Music & Other Facilities</li>
+                <li>Fastest Food Home Delivery</li>
+            </ul>
+            <button class="order-btn">Order Now</button>
+        </div>
+    </div>
+    <div class="delicious-food-section">
+        <h4>Popular Dishes</h4>
+        <h1>Our Delicious <span style="color: orange;">Foods</span></h1>
+        <p>Food is any substance consumed to provide nutritional <br> support for an organism.</p>
+        <button style="background-color: orange; color:white">All</button>
+        <button>Pizza</button>
+        <button>Burger</button>
+        <button>Drink</button>
+        <button>Sandwich</button>
+    </div>
+
+    <div class="second-card-section" style="background-color: rgb(248, 245, 241);">
+        <div class="second-crad-position">
+            <div class="col-s-3 second-card">
+                <button>-15%</button>
+                <img src="https://codewithsadee.github.io/foodie/assets/images/food-menu-1.png" alt="">
+                <p>Chicken <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                </p>
+                <h2>Fried Chicken Unlimited</h2>
+                <p><b>PRICE:</b> <span style="font-weight: bold;"> $49.00 </span> <strike> $69.00 </strike></p>
+            </div>
+            <div class="col-s-3 second-card">
+                <button>-10%</button>
+                <img src="https://codewithsadee.github.io/foodie/assets/images/food-menu-2.png" alt="">
+                <p>Noddles <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                </p>
+                <h2>Burger King Whopper</h2>
+                <p><b>PRICE:</b> <span style="font-weight: bold;"> $29.00 </span> <strike> $39.00 </strike></p>
+            </div>
+            <div class="col-s-3 second-card">
+                <button>-25%</button>
+                <img src="https://codewithsadee.github.io/foodie/assets/images/food-menu-3.png" alt="">
+                <p>Pizzas <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                </p>
+                <h2>White Castle Pizzas</h2>
+                <p><b>PRICE:</b> <span style="font-weight: bold;"> $49.00 </span> <strike> $69.00 </strike></p>
+            </div>
+            <div class="col-s-3 second-card">
+                <button>-20%</button>
+                <img src="https://codewithsadee.github.io/foodie/assets/images/food-menu-4.png" alt="">
+                <p>Burrito <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                </p>
+                <h2>Bell Burrito Supreme</h2>
+                <p><b>PRICE:</b> <span style="font-weight: bold;"> $59.00 </span> <strike> $69.00 </strike></p>
+            </div>
+            <div class="col-s-3 second-card">
+                <button>-5%</button>
+                <img src="https://codewithsadee.github.io/foodie/assets/images/food-menu-5.png" alt="">
+                <p>Nuggets<span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                </p>
+                <h2>Kung Pao Chicken BBQ</h2>
+                <p><b>PRICE:</b> <span style="font-weight: bold;"> $52.00 </span> <strike> $69.00 </strike></p>
+            </div>
+            <div class="col-s-3 second-card">
+                <button>-15%</button>
+                <img src="https://codewithsadee.github.io/foodie/assets/images/food-menu-6.png" alt="">
+                <p>Chicken <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                </p>
+                <h2>Wendy's Chicken</h2>
+                <p><b>PRICE:</b> <span style="font-weight: bold;"> $49.00 </span> <strike> $69.00 </strike></p>
+            </div>
+        </div>
+    </div>
+
+    <main style="background-image: url(https://codewithsadee.github.io/foodie/assets/images/hero-bg.jpg);">
+        <div class="col-s-4 burgar-details">
+            <h1>The Foodie Have Excellent <br> Of <span style="color: orange;"> Quality Burgers!</span></h1>
+            <p>The restaurants in Hangzhou also catered to many northern Chinese who had fled south from Kaifeng during the Jurchen invasion of the 1120s, while it is also known that many restaurants were run by families.</p>
+            <button class="order-btn">Order Now</button>
+        </div>
+        <div class="col-s-6 image-burgar-section">
+            <img src="https://codewithsadee.github.io/foodie/assets/images/cta-banner.png" alt="Big burgar section">
+        </div>
+    </main>
+
+    <div class="order-section">
+        <div class="col-s-5 order-details">
+            <h1>A Moments Of Delivered On <span style="color: orange;">Right Time</span> & Place</h1>
+            <p>The restaurants in Hangzhou also catered to many northern Chinese who had fled south from Kaifeng during the Jurchen invasion of the 1120s, while it is also known that many restaurants were run by families.</p>
+            <button class="order-btn">Order Now</button>
+        </div>
+        <div class="col-s-5 order-image-section" style="background-image: url(https://codewithsadee.github.io/foodie/assets/images/delivery-banner-bg.png);">
+            <img src="https://codewithsadee.github.io/foodie/assets/images/delivery-boy.svg" alt="">
+        </div>
+    </div>
+    <div class="reviews-section">
+        <h3>Testimonials</h3>
+        <h1>Our Customers <span style="color: orange;">Reviews</span></h1>
+        <p>Food is any substance consumed to provide nutritional <br> support for an organism.</p>
+    </div>
+    <div class="reviwes-card-style" style="background-color: rgb(248, 245, 241);">
+        <div class="col-s-3 reviews-card-section">
+            <div class="reviews-card">
+                <div class="reviews-imgage">
+                    <img src="https://codewithsadee.github.io/foodie/assets/images/avatar-1.jpg" width="80" height="80" alt="Reviews image section">
+                </div>
+                <div class="reviews-name">
+                    <h3>Robert William</h3>
+                    <p>CEO Kingfisher</p>
+                </div>
+            </div>
+            <div class="reviews-details">
+                <p>"I would be lost without restaurant. I would like to personally thank you for your outstanding product."
+                </p>
+                <div class="star-section">
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-s-3 reviews-card-section">
+            <div class="reviews-card">
+                <div class="reviews-imgage">
+                    <img src="https://codewithsadee.github.io/foodie/assets/images/avatar-2.jpg" width="80" height="80" alt="Reviews image section">
+                </div>
+                <div class="reviews-name">
+                    <h3>Thomas Josef</h3>
+                    <p>CEO Getforce</p>
+                </div>
+            </div>
+            <div class="reviews-details">
+                <p>"I would be lost without restaurant. I would like to personally thank you for your outstanding product."
+                </p>
+                <div class="star-section">
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-s-3 reviews-card-section">
+            <div class="reviews-card">
+                <div class="reviews-imgage">
+                    <img src="https://codewithsadee.github.io/foodie/assets/images/avatar-3.jpg" width="80" height="80" alt="Reviews image section">
+                </div>
+                <div class="reviews-name">
+                    <h3>Charles Richard</h3>
+                    <p>CEO Angela</p>
+                </div>
+            </div>
+            <div class="reviews-details">
+                <p>"I would be lost without restaurant. I would like to personally thank you for your outstanding product."
+                </p>
+                <div class="star-section">
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="discount-food-section">
+        <div class="col-s-5 discount-ludus" style="background-image: url(https://codewithsadee.github.io/foodie/assets/images/banner-1.jpg);">
+            <h1>50% Off Now!</h1>
+            <h1>Discount For Delicious <br> Tasty Burgers!</h1>
+            <p>Sale off 50% only this week</p>
+            <button class="order-btn">Order Now</button>
+        </div>
+        <div class="col-s-2 discount-pizza">
+            <img src="https://codewithsadee.github.io/foodie/assets/images/banner-2.jpg" alt="">
+            <div class="discount-details">
+                <h1>Delicious <br> Pizza</h1>
+                <p>50% off Now</p>
+                <button class="order-btn">Order Now</button>
+            </div>
+        </div>
+        <div class="col-s-2 discount-pizza">
+            <img src="https://codewithsadee.github.io/foodie/assets/images/banner-3.jpg" alt="">
+            <div class="discount-details">
+                <h1>American <br> Burgers</h1>
+                <p>50% off Now</p>
+                <button class="order-btn">Order Now</button>
+            </div>
+        </div>
+        <div class="col-s-6 discount-tasty-pazza">
+            <img src="https://codewithsadee.github.io/foodie/assets/images/banner-4.jpg" alt="">
+            <div class="discount-detail">
+                <h1>Tasty Buzzed <br> Pizza</h1>
+                <p>Sale off 50% only this week</p>
+                <button class="order-btn">Order Now</button>
+            </div>
+        </div>
+    </div>
+    <div class="all-about-food-section">
+        <div class="all-about-details">
+            <b>Latest Blog Posts</b>
+            <h1>This Is All About <span style="color: orange;">Foods</span></h1>
+            <p>Food is any substance consumed to provide nutritional <br> support for an organism.</p>
+        </div>
+        <div class="col-s-3 food-card">
+            <div class="food-card-pizza">
+                <img src="https://codewithsadee.github.io/foodie/assets/images/blog-1.jpg" alt="Food image">
+            </div>
+            <div class="btn-pizza">
+                <button>PIZZA</button>
+            </div>
+            <div class="card-content">
+                <h1>What Do You Think About <br>Cheese Pizza Recipes</h1>
+                <p>Financial experts support or help you to to <br> find out which way you can raise your funds <br> more...</p>
+                <b>read more <i class="fa-sharp fa-solid fa-arrow-right"></i></b>
+            </div>
+        </div>
+        <div class="col-s-3 food-card">
+            <div class="food-card-pizza">
+                <img src="https://codewithsadee.github.io/foodie/assets/images/blog-2.jpg" alt="Food image">
+            </div>
+            <div class="btn-pizza">
+                <button>BURGER</button>
+            </div>
+            <div class="card-content">
+                <h1>Making Chicken Strips With <br> New Delicious Ingridents.</h1>
+                <p>Financial experts support or help you to to <br> find out which way you can raise your funds <br> more...</p>
+                <b>read more <i class="fa-sharp fa-solid fa-arrow-right"></i></b>
+            </div>
+        </div>
+        <div class="col-s-3 food-card">
+            <div class="food-card-pizza">
+                <img src="https://codewithsadee.github.io/foodie/assets/images/blog-3.jpg" alt="Food image">
+            </div>
+            <div class="btn-pizza">
+                <button>CHICKEN</button>
+            </div>
+            <div class="card-content">
+                <h1>Innovative Hot Chessyraw <br> Pasta Make Creator Fact.</h1>
+                <p>Financial experts support or help you to to <br> find out which way you can raise your funds <br> more...</p>
+                <b>read more <i class="fa-sharp fa-solid fa-arrow-right"></i></b>
+            </div>
+        </div>
+    </div>
+    
+    
+</body>
+
+</html>
